be911421311cc8ac3b2297a9d74eba2d43669f9e
SangKim <batardeau@gmail.com>
2019-03-15 00:21:57 -0400

    change password

---
+++ db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb+srv://zuko84:${password}@cluster0-yq0wg.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true });
+
+module.exports = {mongoose};
+
